ae6c9c77c9c4be76b0ad03a0dd8b54e8646d87c9
Gabriel Chuang <chickenricecesium@gmail.com>
2017-05-10 09:30:30 -0700

    First Commit, testing.

---
+++ Documents/Gabriel/11thGradeWork/Academic/CS/project/CSVReader.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class CSVReader {
+	public static void main(String[] args) throws FileNotFoundException {
+		ArrayList<ArrayList<Double>> test = new ArrayList<ArrayList<Double>>();
+		test = readFile("powerspec/s01.powerspec.csv");
+		System.out.println(test.get(2).get(100));
+	}
+	public static ArrayList<ArrayList<Double>> readFile(String a) throws FileNotFoundException {
+		ArrayList<ArrayList<Double>> toreturn = new ArrayList<ArrayList<Double>>();
+		File f = new File(a);
+		Scanner sc = new Scanner(f);
+		String trash = sc.nextLine();
+		int count = 0;
+		trash = sc.next();
+		while (sc.hasNext()) {
+			ArrayList<Double> toadd = new ArrayList<Double>();
+			while (true) {
+				String l = ":(";
+				try {l = sc.next();} 
+				catch (NoSuchElementException e) {break;}//nothing more to read in entire file
+				try {toadd.add(Double.parseDouble(l.substring(0,l.length()-1)));} 
+				catch (NumberFormatException e) {//end of line
+					toreturn.add(toadd);
+					break;
+				}
+			}			
+		}
+	   /* System.out.println(toreturn.size());
+		* System.out.println(toreturn.get(0).size());
+		* for (int i=0;i<5;i++) {
+		*	System.out.println(toreturn.get(i).get(toreturn.get(i).size()-2));
+		* }
+		*/
+		return toreturn;
+	}
+}
