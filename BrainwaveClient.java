2378d442733bc34bcd4decee00922885cd4cdb44
Gabriel Chuang <chickenricecesium@gmail.com>
2017-05-16 20:58:38 -0700

    added cosine sim and associated vector ops

--- BrainwaveClient.java
+++ BrainwaveClient.java
@@ -5,8 +5,16 @@ public class BrainwaveClient {
 	public static void main(String[] args) {
 		CSVReader c = new CSVReader();
 		try {
-			PowerSpectrum p = new PowerSpectrum(c.readFile("powerspec/s01.powerspec.csv"));
+			PowerSpectrum p = new PowerSpectrum(c.readFile("powerspec/s21.powerspec.csv"));
+			PowerSpectrum q = new PowerSpectrum(c.readFile("powerspec/s22.powerspec.csv"));
 			System.out.println(p.getVal(10, 11));
+			for (int i=0;i<192;i++) {
+			//	System.out.print("("+i+",");
+			//	System.out.print(p.similarity(q, i, "cosine"));
+			//	System.out.print("),");
+				System.out.print(q.getVal(3, i));
+				System.out.print(", ");
+			}
 		} catch (FileNotFoundException e) {
 			//do nothing
 		}
--- PowerSpectrum.java
+++ PowerSpectrum.java
@@ -1,19 +1,41 @@
 import java.util.*;
 public class PowerSpectrum {
 	private ArrayList<ArrayList<Double>> myPowSpec;
-	public PowerSpectrum(ArrayList<ArrayList<Double>> n) {
+
+	public PowerSpectrum(ArrayList<ArrayList<Double>> n) { //constructor: sets myPowSpec to the passed 2d arraylist
 		myPowSpec = n;
 	}
-	public double getVal(int time, int coef) {
+	public double getVal(int time, int coef) { //gets a value
 		return myPowSpec.get(time).get(coef);
 	}
-	public ArrayList<Double> getTimeCol(int time) {
+	public ArrayList<Double> getTimeCol(int time) { //gets a single time snapshot vector
 		return myPowSpec.get(time);
 	}
-	public double similarity(PowerSpectrum other, int time, String type) {
+	public double similarity(PowerSpectrum other, int time, String type) { //returns similarity of a certain type at a certain time
 		if (type.equals("cosine")) {
 			ArrayList<Double> first = new ArrayList<Double>();
+			ArrayList<Double> second = new ArrayList<Double>();
+			first = this.getTimeCol(time);
+			second = other.getTimeCol(time);
+			return dotProduct(first, second) / (vectMagnitude(first) * vectMagnitude(second));
 		}
-		return 1;
+		return 0;
+	}
+	private double dotProduct(ArrayList<Double> a, ArrayList<Double> b) { //vector operation used for cosine similarity
+		if (a.size() == b.size()) {
+			double total = 0;
+			for (int i=0;i<a.size();i++) {
+				total += a.get(i) * b.get(i);
+			}
+			return total;
+		} 
+		return 0;
+	}
+	private double vectMagnitude(ArrayList<Double> a) { //vector operation used for cosine similarity
+		double total = 1;
+		for (int i=0;i<a.size();i++) {
+			total += a.get(i) * a.get(i);
+		}
+		return Math.sqrt(total);
 	}
 }
