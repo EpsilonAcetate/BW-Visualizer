cb365d2ed6ea8d2e8db9f88a92534758538c08a4
Prasanna Guthy <guthy@prasana.mission.fmtusd.org>
2017-05-22 11:47:38 -0700

    just forgot to add updated client

--- BrainwaveClient.java
+++ BrainwaveClient.java
@@ -1,8 +1,20 @@
 import java.io.*;
-import java.util.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class BrainwaveClient {
+public class BrainwaveClient extends Application{
+    @Override
+         public void start(Stage primaryStage) throws IOException {
+            /*generic container class*/Parent root = FXMLLoader.load(getClass().getResource("BWVisualizerFXMLVer2.fxml"));//refer to indicated fxml file for interface modification
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
 	public static void main(String[] args) {
+                launch(args);
 		CSVReader c = new CSVReader();
 		try {
 			PowerSpectrum p = new PowerSpectrum(c.readFile("powerspec/s21.powerspec.csv"));
