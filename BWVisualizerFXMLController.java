400b15b747fc8863a19cac3d5b81b1130f81f36e
Prasanna Guthy <guthy@prasana.mission.fmtusd.org>
2017-05-22 11:46:24 -0700

    modified BrainWave client to also launch graphical application; added fxml file with interface design; added fxmlController java file which is mostly empty but it will contain the behavior for the buttons, textfields etc.

---
+++ BWVisualizerFXMLController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bw.visualizer.fxml.ver1.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author guthy
+ */
+public class BWVisualizerFXMLController implements Initializable {
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction1(ActionEvent event) {
+        label.set("Running Similarity Test");
+        
+         
+   }
+    
+    @FXML
+    private void handleButtonAction2(ActionEvent event){
+        
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
