8f1a3c02aed2522daee64083e3d9a8b26bc69d47
Gabriel Chuang <chickenricecesium@gmail.com>
2017-06-04 16:07:48 -0700

    saving in order to pull

--- BWVisualizerFXMLController.java
+++ BWVisualizerFXMLController.java
@@ -66,91 +66,17 @@ public class BWVisualizerFXMLController implements Initializable {
     @FXML
     private void handleTest2ButtonAction(ActionEvent event) {
         System.out.println("Button 2 has been pressed.");
-        handleGraph();
+        handleGraph1();
     }
     
     @FXML
     private void handleTest3ButtonAction(ActionEvent event){
-        titleText.setText("New Title YAY");
+        //titleText.setText("New Title YAY");
     }
 
     int q=0;
     @FXML
-    private void handleGraph() {
-        //do things
-        /*try {
-            Stage stage = new Stage();
-            CSVReader c = new CSVReader();
-            PowerSpectrum p = new PowerSpectrum(c.readFile("powerspec/s21.powerspec.csv"));
-            PowerSpectrum p2 = new PowerSpectrum(c.readFile("powerspec/s12.powerspec.csv"));
-            stage.setTitle("Brainwaves!");
-            final NumberAxis xAxis = new NumberAxis();
-            final NumberAxis yAxis = new NumberAxis();
-            xAxis.setAutoRanging(false);
-            xAxis.setLowerBound(0);
-            xAxis.setUpperBound(180);
-            xAxis.setTickUnit(10);
-
-            yAxis.setAutoRanging(false);
-            yAxis.setLowerBound(0);
-            yAxis.setUpperBound(10);
-            yAxis.setTickUnit(2);
-            xAxis.setLabel("Power");
-            yAxis.setLabel("Coefficient");
-            final LineChart<Number,Number> lineChart =  new LineChart<Number,Number>(xAxis,yAxis);
-            lineChart.setTitle("Brainwave sample visualization at t=0");
-            Scene scene  = new Scene(lineChart,1200,800);
-            lineChart.setLegendVisible(false);
-            graph1 = lineChart;
-            stage.setScene(scene);
-            stage.show();
-            System.out.println(p.getLength());
-            Timeline tl = new Timeline();
-            try {
-                System.out.println("hello");
-                tl.getKeyFrames().add(
-                    new KeyFrame(Duration.millis(50), 
-                            new EventHandler<ActionEvent>() {
-                            @Override public void handle(ActionEvent actionEvent) {
-                                XYChart.Series series3 = new XYChart.Series();
-                                for (int i=0;i<170;i+=4) {
-                                    series3.getData().add(new XYChart.Data(i, p.getVal(q, i)));
-                                }
-                                graph1.getData().add(series3);
-                                q++;
-                                series3.getData().clear();
-                            }
-                         }
-                        ));
-                    tl.setCycleCount(Animation.INDEFINITE);
-                    tl.setAutoReverse(true);
-                    tl.play();
-            } catch (Exception e) {
-                //do nada
-            }
-        } catch (FileNotFoundException e) {
-            //do nothing
-        }*/
-        
-
-        /*try {
-            
-            for (int j=0;j<5;j++) {
-                XYChart.Series series0 = new XYChart.Series();
-                for (int i=0;i<170;i+=4) {
-                    series0.getData().add(new XYChart.Data(i, p.getVal(j, i)));
-                }
-                graph1.getData().add(series0);
-                try {Thread.sleep(1000);}
-                catch (InterruptedException e) {System.out.println("DO NOT INTERRUPT ME.");}
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("there was a filenotfoundexception!!!!111!");
-        }*/
-
-
-
-
+    private void handleGraph1() {
         Timeline tl = new Timeline();
         try {
             CSVReader c = new CSVReader();
